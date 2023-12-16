ede341289b956cd5651edb8c0a053966272217c6
klauSigma <113836234+klauSigma@users.noreply.github.com>
2023-12-16 08:40:46 +0200

    Released this version

--- Controllers/ParseController.cs
+++ Controllers/ParseController.cs
@@ -1,5 +1,4 @@
-﻿using MdToHtml;
-
+﻿
 using Microsoft.AspNetCore.Mvc;
 
 using Newtonsoft.Json;
@@ -16,9 +15,7 @@ public class ParseController : ControllerBase
         {
             var escapedForJson = await Task.Run(() =>
              {
-                 var parsedLines = Parser.Process(content);
-                 var parsedContent = string.Join(Environment.NewLine, parsedLines);
-                 return JsonConvert.SerializeObject(parsedContent);
+                 return JsonConvert.SerializeObject("");
              });
 
             return Ok(escapedForJson);
